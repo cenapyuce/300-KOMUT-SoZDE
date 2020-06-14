8c7846eed06072a9199c087b2a6f3d272a44f7c2
cenapyuce <41297513+cenapyuce@users.noreply.github.com>
2020-06-14 21:51:08 +0300

    KOMUT TASLAKI

---
+++ komut taslağı.js
@@ -0,0 +1,25 @@
+﻿const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+
+
+};
+
+exports.conf = {
+  enabled: true, //komut açık kapalı
+  guildOnly: false, //herkese kullanır
+  aliases: ['1', '2'], //komutun farklı kullanımları
+  permLevel: 0 //kimler kullancağını belirtir 
+};
+
+exports.help = {
+  name: 'komutadı', //adını belirtin (kullanmak için gereken komut) Örneğin otorol
+  description: 'komutaçıklaması', //komutun açıklaması
+  usage: 'kullanımı' //komutun kullanım şekli (örneğin x!otorol <@rol> <#kanal>)
+};
+
+
+
+
+//Bu komut taslağı Seinroth#8230 tarafından yazılmıştır.//
